e3a24181a996a4ea147224d9dfcd613a031fb626
spencer handley <spencerhandley@gmail.com>
2014-05-25 12:18:04 -0700

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular
+  .module('gameonApp', [
+    'ngCookies',
+    'ngSanitize'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('gameonApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
